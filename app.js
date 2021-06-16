36c33097b0945b9fc9becf46fe13ce9899a8c9f3
yahya ganjo <yahya.ganjo@gmail.com>
2021-06-16 02:10:07 +0200

    app finished

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ window.addEventListener("load", () => {
       lat = position.coords.latitude;
 
       const proxy = "https://cors-anywhere.herokuapp.com/";
-      const api = `${proxy}http://api.weatherapi.com/v1/current.json?key=edd7b783b54148fea28222124211506&q=${lat},${long}&aqi=no`;
+      const api = `http://api.weatherapi.com/v1/current.json?key=edd7b783b54148fea28222124211506&q=${lat},${long}&aqi=no`;
 
       fetch(api)
         .then((response) => response.json())
